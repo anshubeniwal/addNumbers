991c5adb10f482a779dbe82be52850ddfe45e428
anshubeniwal <anshubeniwal7@gmail.com>
2021-06-10 10:46:43 +0530

    DRY

---
+++ test.js
@@ -0,0 +1,13 @@
+import { addNumbers } from 'numbers-add'
+
+let addition1 = addNumbers(1, 2)
+
+console.log(addition1)   // => 3
+
+let addition2 = addNumbers(1, 2, 5, 7)
+
+console.log(addition2)   // => 15
+
+let addition3 = addNumbers(12, 2, 9, 10, 1)
+
+console.log(addition3)   // => 34
